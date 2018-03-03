3c4c6bca0a24e8bc76011c7c3e0b59a6fec023ea
AFeas1987 <majestic254@gmail.com>
2018-03-03 09:18:48 -0500

    Base program launches ODK Collect as Intent.

---
+++ app/src/main/java/com/example/nccnestapp/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.nccnestapp;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void mainButtonClick(View v){
+        Intent i = new Intent (Intent.ACTION_VIEW);
+        i.setType("vnd.android.cursor.dir/vnd.odk.form");
+        startActivity(i);
+    }
+}
